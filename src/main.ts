c12bcaa56f44b02c1d54af3f3fc036329623a759
chen <wszgrcy@gmail.com>
2025-08-13 21:08:13 +0800

    chore: load page

--- src/main.ts
+++ src/main.ts
@@ -14,19 +14,19 @@ const lang = localStorage.getItem('lang')!;
 if (lang === 'zh-hans') {
   setGlobalConfig({ lang: 'zh-CN' });
 }
-fetch(`i18n/${lang}.json`)
-  .then((a) => a.json())
-  .then((data) => {
-    loadTranslations(data);
-  })
-  .catch((rej) => {
-    console.error(rej);
-    loadTranslations({});
-  })
-  .then(async () => {
-    const [{ App }, { appConfig }] = await Promise.all([
-      import('./app/app'),
-      import('./app/app.config'),
-    ]);
-    bootstrapApplication(App, appConfig).catch((err) => console.error(err));
-  });
+Promise.all([import('./app/app'), fetch(`i18n/${lang}.json`)]).then(
+  ([{ App }, data]) =>
+    data
+      .json()
+      .then((data) => {
+        loadTranslations(data);
+      })
+      .catch((rej) => {
+        console.error(rej);
+        loadTranslations({});
+      })
+      .then(async () => {
+        const { appConfig } = await import('./app/app.config');
+        bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+      }),
+);
